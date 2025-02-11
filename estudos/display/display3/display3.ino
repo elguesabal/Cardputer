bce03bb0894e66fdfb7f5c4fb8873f0a657bc0e9
elguesabal <joseelguesabal@gmail.com>
2025-02-11 17:00:57 -0300

    adicionando testes com display e inputs

--- estudos/display/display3/display3.ino
+++ estudos/display/display3/display3.ino
@@ -1,5 +1,28 @@
 #include <M5Cardputer.h>
 
+void clearBackgrownd(void) {
+    M5Cardputer.Display.fillRect(0, 0, M5Cardputer.Display.width(), M5Cardputer.Display.height(), BLACK);
+}
+
+void circle(int &x) {
+    M5Cardputer.Display.fillCircle(x, 72, 10, GREEN);
+}
+
+void shadow(int &x, bool direction, int amount) {
+    for (unsigned int i = 1; i - 1 < amount; ++i) {
+        M5Cardputer.Display.drawCircle(x + i * (direction ? -20 : 20), 72, 10, GREEN);
+    }
+}
+
+void redirection(int *x, bool *direction) {
+    if (*x == M5Cardputer.Display.width()) {
+        *direction = false;
+    } else if (*x == 0) {
+        *direction = true;
+    }
+    *x += (*direction ? 10 : -10);
+}
+
 void setup(void) {
     M5Cardputer.begin(M5.config());
 }
@@ -8,19 +31,10 @@ void loop(void) {
     static int x = 0;
     static bool direction = true;
 
-    M5Cardputer.Display.fillRect(0, 0, M5Cardputer.Display.width(), M5Cardputer.Display.height(), BLACK);
-    M5Cardputer.Display.fillCircle(x, 72, 10, GREEN);
-    x += (direction ? 10 : -10);
+    clearBackgrownd();
+    circle(x);
+    shadow(x, direction, 3);
+    redirection(&x, &direction);
 
-    if (x == M5Cardputer.Display.width()) {
-        direction = false;
-    } else if (x == 0) {
-        direction == true;
-    }
-
-    char width[10];
-    itoa(M5Cardputer.Display.width(), width, 10);
-    M5.Display.println(width);
-
-    delay(500);
+    delay(100);
 }
---
+++ estudos/inputs/inputs1/inputs1.ino
@@ -0,0 +1,31 @@
+#include <M5Cardputer.h>
+
+void setup(void) {
+    M5Cardputer.begin();
+    M5Cardputer.Display.setTextColor(GREEN);
+    M5Cardputer.Display.setTextSize(2);
+    M5Cardputer.Display.setTextDatum(middle_center);
+}
+
+void loop(void) {
+    M5Cardputer.Display.fillRect(0, 0, M5Cardputer.Display.width(), M5Cardputer.Display.height(), BLACK);
+    M5Cardputer.update();
+
+    if (M5Cardputer.BtnA.wasPressed()) {
+        M5Cardputer.Display.drawString("Botao A foi pressionado!", M5Cardputer.Display.width() / 2, M5Cardputer.Display.height() / 2);
+    }
+
+    if (M5Cardputer.BtnA.isPressed()) {
+        M5Cardputer.Display.drawString("Botao A esta sendo segurado!", M5Cardputer.Display.width() / 2, M5Cardputer.Display.height() / 2);
+    }
+
+    if (M5Cardputer.BtnA.wasReleased()) {
+        M5Cardputer.Display.drawString("Botao A foi solto!", M5Cardputer.Display.width() / 2, M5Cardputer.Display.height() / 2);
+    }
+
+    if (M5Cardputer.BtnA.pressedFor(5000)) {
+        M5Cardputer.Display.drawString("Botao A foi segurado por 1 segundo!", M5Cardputer.Display.width() / 2, M5Cardputer.Display.height() / 2);
+    }
+
+    delay(500);
+}
